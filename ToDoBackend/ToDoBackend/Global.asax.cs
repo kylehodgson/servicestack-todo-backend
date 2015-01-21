d75045f0061f61f84c29a7168ad99ec20a36f853
Kyle Hodgson <kyle.hodgson@gmail.com>
2015-01-21 10:47:50 -0500

    cors options...

--- ToDoBackend/ToDoBackend/Global.asax.cs
+++ ToDoBackend/ToDoBackend/Global.asax.cs
@@ -9,7 +9,6 @@ namespace ToDoBackend
 {
     public class Global : System.Web.HttpApplication
     {
-
         protected void Application_Start(object sender, EventArgs e)
         {
             (new AppHost()).Init();
